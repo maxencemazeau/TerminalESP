68f4cea8a20ad27e18c85ad3bde56964886cff57
maxencemazeau <89794067+maxencemazeau@users.noreply.github.com>
2023-04-29 17:13:09 -0400

    Fin commentaire

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,9 @@
 
     Historique des versions
            Version    Date       Auteur       Description
-           1.1        05/08/21  Maxence     Première version
-           1.2                  Maxence     Deuxième version
-           1.3       30/40/2023 Maxence     Troisième version
+           1.1        16/02/23  Maxence     Première version
+           1.2        23/04/23  Maxence     Deuxième version
+           1.3       30/40/23   Maxence     Troisième version
    
  * */
 
